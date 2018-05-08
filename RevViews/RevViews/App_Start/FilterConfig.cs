a1942008d7dd67ffd8fa58ff5d506373480a81b9
Donald Clements <silveranstavern@yahoo.com>
2018-05-08 13:37:12 -0400

    Failed at creating custom 404 page, added bootswatches, edited Contact Us page.

--- RevViews/RevViews/App_Start/FilterConfig.cs
+++ RevViews/RevViews/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace RevViews
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            
         }
     }
 }
